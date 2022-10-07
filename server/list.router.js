ce4facc82a169f4a45c0db4efe75c08225acb3f6
Lydia Wildes <lydiawildes1@gmail.com>
2022-10-07 17:25:56 -0500

    create delete function

--- server/list.router.js
+++ server/list.router.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {
     INSERT INTO "list"
     ("task")
     VALUES
-    ($1);`;
+    ($1);`; //safety measure to stop sneaky behavior
    
     const sqlParams = [
         req.body.task
@@ -31,15 +31,52 @@ router.get('/', (req,res) => {
 
     pool.query(`
     SELECT * FROM "list"
-    ORDER BY "id" ASC;`
+    ORDER BY "id" ASC;` //keeps tasks in order added even after clicking buttons
     )
     .then((dataBaseResult) => {
-        res.send(dataBaseResult.rows);
+        res.send(dataBaseResult.rows); //we just need to send the table row data
     })
     .catch((err) => {
         console.log('in /list router GET error', err);
         res.sendStatus(500);
     })
+})
+
+router.put('/:id', (req, res) => {
+    console.log('in list PUT with id of', req.params.id);
+    let queryParams = [req.params.id];
+
+    let queryText = `UPDATE "list" 
+                    SET "completed" = NOT "completed" 
+                    WHERE "id" = $1;`
+
+    pool   
+        .query(queryText, queryParams)
+        .then((result) => {
+            console.log('task saved as completed!');
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('error completing task', err);
+            res.sendStatus(500);
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    console.log('in delete with id: ', req.params.id);
+    const taskID = req.params.id;
+    const sqlText =`DELETE FROM "list" WHERE "id" = $1;`
+    const sqlParams = [taskID];
+
+    pool
+        .query(sqlText, sqlParams)
+        .then((databaseResult) =>{
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('DELETE failed', err);
+            res.sendStatus(500);
+        })
 })
 
 
--- server/public/client.js
+++ server/public/client.js
@@ -1,14 +1,20 @@
 
 
-console.log('in client.js')
+console.log('in client.js');
 
 $( document ).ready(function(){
-    console.log('JQ')
+    console.log('JQ');
 
+    //render current list to DOM
     getList();
-    $('#addBtn').on('click', addTask)
+    //on click of add button call addTask function
+    $('#addBtn').on('click', addTask);
+    //on click of buttons that WILL exist but don't yet
+    $('body').on('click', '#completeBtn', markComplete);
+    $('body').on('click', '#deleteBtn', deleteTask);
 });
 
+// send AJAX request to router 
 function addTask() {
     const taskToSend = {
         task: $('#taskInput').val()
@@ -18,7 +24,7 @@ function addTask() {
     $.ajax({
         method:'POST',
         url:'/list' ,
-        data: taskToSend
+        data: taskToSend //variable containing our task input data from client
     })
     .then((response) => {
         console.log(response);
@@ -29,23 +35,68 @@ function addTask() {
     })
 }
 
-function getList(){
-    console.log('Here is the to do list:')
+//function to render list on ready and after add/complete/delete
+function getList() {
 
     $.ajax({
         method:'GET',
         url: '/list'
     })
-    .then((response) =>{
+    .then((response) =>{                    //response = list.rows
         console.log('response from database is:', response);
-
+        $('#listTable').empty(); //empty before appending updated data
+        //loop through all tasks in database and append with buttons
         for(chore of response){
+            
             $('#listTable').append(`
             <tr>
                 <td>${chore.task}</td>
+                <td>
+                    <button class="btn" id="completeBtn" data-id=${chore.id} >Complete</button>
+                </td>
+                <td>
+                    <button class="btn" id="deleteBtn" data-id=${chore.id} >Delete</button>
+                </td>
             </tr>
         `)
         }
         
+    })
+}
+
+function markComplete() {
+    console.log('client marked task as complete 🥳');
+    //get the specific task ID
+    const taskID = $(this).data('id');
+    console.log('TESTING FOR DATA', $(this).data('id'))
+    //PUT request to change status of completed from false to true
+    $.ajax({
+        method:'PUT',
+        url: `/list/${taskID}`
+    })
+    .then((res) => {
+        console.log('task saved as complete ✔️')
+        getList();
+    })
+    .catch((err) => {
+        console.log('PUT list error', err);
+    })
+
+}
+
+function deleteTask() {
+    const taskID = $(this).data('id');
+    console.log('TESTING FOR DATA TO DELETE', $(this).data('id'))
+
+    $.ajax({
+        method:'DELETE',
+        url: `/list/${taskID}`
+    })
+    .then((res) => {
+        console.log('task successfully deleted!');
+        getList(); //render update to DOM
+    })
+    .catch((err) => {
+        console.log('DELETE error', err);
     })
 }
